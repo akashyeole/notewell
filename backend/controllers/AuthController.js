06b4f57e1753de890dc20603fe35b7996784b672
Akash <akashyeole02@gmail.com>
2023-05-09 00:18:34 +0530

    Added hashing functionality for storing passwords and Added support for returing JWT tokends on successfull resgisteration of a user.

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,5 +1,7 @@
 const User  = require("../models/User");
 const {validationResult} = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Controller to register new user
 const registerUser = (req, res)=>{
@@ -7,16 +9,23 @@ const registerUser = (req, res)=>{
     // Check fields validation
     if(errors.isEmpty()){
         // Check if user already exists
-        User.findOne({email: req.body.email}).then((isFound)=>{
+        User.findOne({email: req.body.email}).then(async (isFound)=>{
             // If yes send error
             if(isFound){
                 res.status(400).json({errors: [{type: "db", msg: "User already registered with this email"}]});
             }else{
                 // Otherwise add user
-                User.create(
-                    req.body
-                ).then((user)=>{
-                    res.status(200).json(user);
+                // salting and hashing
+                const salt = await bcrypt.genSalt(10);
+                const hash = await bcrypt.hash(req.body.password, salt);
+                User.create({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hash
+                }).then((user)=>{
+                    // creating authToken
+                    const authToken = jwt.sign({id: user.id}, process.env.JWT_SECRET);
+                    res.status(200).json({authToken});
                 }).catch((err)=>{
                     res.status(400).json({errors: [{type: "db", msg: err.message}]});
                 });
